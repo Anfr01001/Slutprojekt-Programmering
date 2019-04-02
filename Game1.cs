bd49dad3a77b3257cde7d120206da63f39e794f7
James Grimm <bananprivat@gmail.com>
2019-04-02 13:09:36 +0200

    Vecka 14: Spelaren kan nu skjuta

--- Game1.cs
+++ Game1.cs
@@ -2,8 +2,43 @@
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using System;
+using System.Collections.Generic;
 
 namespace Shooter_Game_slutprojekt {
+
+     /// Denna klassen sköter allt gällande skott.
+    class Skott {
+        
+        Texture2D texture;
+        Vector2 pos;
+        float Rotation;
+        Vector2 orgin = new Vector2(13.5f,4f); //-- Hitta mitten av bilden för att vera var skottet ska rotera runt.
+        MouseState MouseState;
+        Vector2 direction;
+        float speed = 5;
+
+    public Skott(Texture2D texture, Vector2 position,float rot, MouseState MouseState) {
+            //När konstruktion kallas vill vi "Få ut variablerna"
+            this.texture = texture;
+            pos = position;
+            Rotation = rot;
+            MouseState = MouseState;
+    }
+
+    public void Update(){
+        // Gör om spelarens nuvarande rotation till en riktning som sedan används för att flytta skotten.
+        direction = new Vector2((float)Math.Cos(Rotation),(float)Math.Sin(Rotation));
+        //Flytta skotten och en variabel för fart för att lätt kunna ändra det.
+        pos -= direction * speed;
+    }
+
+    public void Draw(SpriteBatch spriteBatch) {
+            //Rita ut skottet samma som overloads som spelaren
+            spriteBatch.Draw(texture,pos,null,null,orgin,Rotation);
+        }
+
+    }
+
     public class Game1 : Game {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
@@ -24,6 +59,10 @@ namespace Shooter_Game_slutprojekt {
 
         //Bakgrund 
         Texture2D Bakgrund;
+        
+        //Skott 
+        Texture2D skott;
+        List<Skott> Skottlista = new List<Skott>();
 
         public Game1() {
             graphics = new GraphicsDeviceManager(this);
@@ -48,6 +87,7 @@ namespace Shooter_Game_slutprojekt {
           
             Player = Content.Load<Texture2D>("SpelareXwing"); //-- Temp för spelare
             font = Content.Load<SpriteFont>("Ubuntu32"); //-- Fonten för att skriva ut text
+            skott = Content.Load<Texture2D>("Skott"); //-- Skotten som spelaren skjuter
             Bakgrund = Content.Load<Texture2D>("SpaceBackground"); //-- Bakgrund
         }
 
@@ -65,6 +105,16 @@ namespace Shooter_Game_slutprojekt {
 
             // Vilken punkt ska karaktären rotera runt (mitten av sig själv)
             orgin = new Vector2(Player.Width / 2f, Player.Height / 2f);
+           
+        //Kollar om användaren klickar om detta sker skapa skott 
+        if(MouseState.LeftButton == ButtonState.Pressed) {
+            Skottlista.Add(new Skott(skott,PlayerPos,PlayerRotation,MouseState)); //-- !Fixa vad skottet utgår från!
+        }
+
+        //Uppdatera skott possition
+        for (int j = 0; j < Skottlista.Count; j++) {
+                 Skottlista[j].Update();
+        }
 
             base.Update(gameTime);
         }
@@ -81,8 +131,10 @@ namespace Shooter_Game_slutprojekt {
             // Rita ut text
             spriteBatch.DrawString(font, ("Grader: " + degrees), new Vector2(100, 100), Color.White);
             
-;
-
+            //Rita ut skott
+            foreach (var item in Skottlista) {
+                item.Draw(spriteBatch);
+            }
 
             spriteBatch.End();
             base.Draw(gameTime);
