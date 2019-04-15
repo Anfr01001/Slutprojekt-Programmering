c79554e41e23ef39a60765f4919d98ff741078bf
James Grimm <bananprivat@gmail.com>
2019-04-15 21:54:55 +0200

    Vecka 16: Mer krångel | Skott tas nu bort utanför kartan & om fienden kommer för nära "Träffar" spelaren tas de bort (i föberedelse för en "End screed")

---
+++ Slutprojekt-Programmering/Fiende.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+public class Fiende {
+    Texture2D texture;
+    Vector2 pos;
+    float Rotation;
+    Vector2 orgin = new Vector2(50, 50); //-- Hitta mitten av bilden för att vera var skottet ska rotera runt.
+    Vector2 direction;
+    float speed = 2;
+    double x, y;
+    int angle;
+    Random r = new Random();
+
+    List<Fiende> Fiendelista;
+
+    //hitbox 
+    Rectangle Fiendehitbox;
+
+    Texture2D Blob;
+
+    public Fiende(Texture2D texture, Texture2D Dot, List<Fiende> flista) {
+        this.texture = texture;
+        //----------------------------------------------------------------------------------------------------------
+        //Fienden skapas genom att bestämma en slumpald vinkel på en imaginär cirkel runt (utanför) spelplanen   
+        //För att sedan ta reda på vilken X och Y värde denna plats på cirklen har.
+        //Efter det roteras figuren mot spelaren och börjar fördas med x speed mot denna positon.
+        //----------------------------------------------------------------------------------------------------------
+
+        //Bestämmer en slumpad vinkel som fiende ska skapas från spelaren.
+        angle = r.Next(0, 361);
+
+        // Ta reda på vad X respektive Y måste vara för att denna vinklen ska skapas och radien på cirklen ska vara 700
+        x = 590 + Math.Cos(angle) * 700;
+        y = 590 + Math.Sin(angle) * 700;
+        pos = new Vector2((float)x, (float)y); // gör om det till vector för att kunna användas
+
+        // Ställer in rotation mot spelaren och sedan gör om det till direction som sedan används för att uppdatera postionen i Update
+        Rotation = (float)Math.Atan2(pos.Y - 500, pos.X - 500);
+        direction = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
+
+        Fiendelista = flista;
+
+        //Skapa hitbox
+        Fiendehitbox = new Rectangle((int)pos.X, (int)pos.Y, 70, 70);
+
+        Blob = Dot; // för test
+    }
+
+    public Rectangle getFiendeHitbox() { return Fiendehitbox; }
+
+
+    public void Update(int i) {
+        pos -= direction * speed; // Gå rakt mot spelaren (direction) som är förutbestämd i konstruktorn
+        Fiendehitbox.X = (int)pos.X - 35; // -35 för att få hitboxen imitten av fienden 
+        Fiendehitbox.Y = (int)pos.Y - 35; // -35 för att få hitboxen imitten av fienden 
+
+
+        //Avståndsformeln *Roten ur*(x2 − x1)^2 + (y2 − y1)^2 | Används för att bedömma om en motståndare är nära någ för att anses vara en träff (onödigt att använda hitboxes här)
+        // Vi vet att spelarpositonen är fast och är 490, 490
+        if (Math.Sqrt(Math.Pow(pos.X - 490, 2) + Math.Pow(pos.Y - 490, 2)) < 50) {
+            Fiendelista.RemoveAt(i);
+
+        }
+    }
+
+    public void Draw(SpriteBatch spriteBatch) {
+        //Rita ut skottet samma som overloads som spelaren (för att kunna använda rotation)
+        spriteBatch.Draw(texture, pos, null, null, orgin, Rotation);
+
+        // Visualisera hitboxen med koden under
+        //spriteBatch.Draw(Blob,new Vector2(Fiendehitbox.X - 35,Fiendehitbox.Y - 35),Color.Red);
+    }
+}
---
+++ Slutprojekt-Programmering/Skott.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+
+class Skott {
+
+    Texture2D texture;
+    Vector2 pos;
+    float Rotation;
+    Vector2 orgin = new Vector2(23f, 13f); //-- Hitta mitten av bilden för att vera var skottet ska rotera runt.
+    Vector2 direction;
+    float speed = 6;
+    List<Skott> skottlista;
+
+    //Hitbox
+    Rectangle skotthitbox;
+
+    public Skott(Texture2D texture, Vector2 position, float rot, List<Skott> slista) {
+        //När konstruktion kallas vill vi "Få ut variablerna"
+        this.texture = texture;
+        pos = position;
+        Rotation = rot;
+        skottlista = slista;
+
+        //Skapa hitbox
+        skotthitbox = new Rectangle((int)pos.X, (int)pos.Y, 8, 8);
+    }
+
+    public Rectangle getskottHitbox() { return skotthitbox; }
+
+    public void Update(int i) {
+        // Gör om spelarens nuvarande rotation till en riktning som sedan används för att flytta skotten.
+        direction = new Vector2((float)Math.Cos(Rotation), (float)Math.Sin(Rotation));
+        //Flytta skotten och en variabel för fart för att lätt kunna ändra det.
+        pos -= direction * speed;
+        skotthitbox.X = (int)pos.X - 4;
+        skotthitbox.Y = (int)pos.Y - 4;
+
+        //Avståndsformeln *Roten ur*(x2 − x1)^2 + (y2 − y1)^2 | används för att kolla om skottet är utanför kartan (1500 är utanför kartan med marginal) för att då ta bort det. (För bättre prestanda)
+        if (Math.Sqrt(Math.Pow(pos.X, 2) + Math.Pow(pos.Y, 2)) > 1500) {
+            skottlista.RemoveAt(i);
+        }
+
+    }
+
+    public void Draw(SpriteBatch spriteBatch) {
+        //Rita ut skottet samma som overloads som spelaren
+        spriteBatch.Draw(texture, pos, null, null, orgin, Rotation);
+    }
+
+}
